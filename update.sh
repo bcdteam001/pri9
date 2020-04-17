b3f58cfa110fa5fd0b6f893ec9389af1f8c5b376
bcdteam001 <61662220+bcdteam001@users.noreply.github.com>
2020-04-17 15:13:02 +0700

    Update 17042020
    Add Update Appsync 7.4 version

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-bzip2 -c9k ./Packages > ./Packages.bz2
-printf "Origin: KidsFunny\nLabel: KidsFunny\nSuite: stable\nVersion: 9.0\nCodename: iOS\nArchitecture: iphoneos-arm\nComponents: main\nDescription: KidsFunny 9.0 iOS\nMD5Sum:\n "$(cat ./Packages | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages)" Packages\n "$(cat ./Packages.bz2 | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages.bz2)" Packages.bz2\n" >Release;
+bzip2 -c9k ./Packages > ./Packages.bz2 ./Packages.gz
+printf "Origin: KidsFunny\nLabel: KidsFunny\nSuite: stable\nVersion: 9.0\nCodename: iOS\nArchitecture: iphoneos-arm\nComponents: main\nDescription: KidsFunny 9.0 iOS\nMD5Sum:\n "$(cat ./Packages | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages)" Packages\n "$(cat ./Packages.bz2 | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages.bz2)" Packages.bz2\n "$(cat ./Packages.gz | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages.gz)" Packages.gz\n" >Release;
 exit 0
